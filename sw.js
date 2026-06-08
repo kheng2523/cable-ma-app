370dd21c4b017ace4c72adbd6519660ae9524d2e
kheng2523 <kheng2523@gmail.com>
2026-06-08 23:30:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 const CACHE = 'cable-ma-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
